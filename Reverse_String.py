b66f68e9e5d020476fc4ffdc6364199308355600
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-06-10 12:47:53 +0530

    s=input() for i in range(len(s)-1,-1,-1):     if(s[i]==' '):         c=0         for j in range(i+1,len(s)):             if(s[j]==' '):                 c+=1             if(c==1):                 break             else:                 print(s[j],end='')         print(' ',end='')     if(i==0):         for j in range(0,len(s)):             if(s[j]==' '):                 break             print(s[j],end='')

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,3 +1,17 @@
-a=list(map(str,input().split()))
-for i in range(len(a)-1,-1,-1):
-    print(a[i],end=' ')
+s=input()
+for i in range(len(s)-1,-1,-1):
+    if(s[i]==' '):
+        c=0
+        for j in range(i+1,len(s)):
+            if(s[j]==' '):
+                c+=1
+            if(c==1):
+                break
+            else:
+                print(s[j],end='')
+        print(' ',end='')
+    if(i==0):
+        for j in range(0,len(s)):
+            if(s[j]==' '):
+                break
+            print(s[j],end='')
