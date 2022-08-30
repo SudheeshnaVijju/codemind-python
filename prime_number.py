e7bf6d1cbedeb8530bd72e3a065b09bb2100eb7e
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-08-30 18:36:50 +0530

    n=int(input()) if(n==1):     print('not a prime') else:     for i in range(2,int(n**0.5)+1):         if(n%i==0):             print('not a prime')             break     else:         print('prime')

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,10 @@
 n=int(input())
-c=0
-for i in range(1,n):
-    if(n%i==0):
-        c+=1
-if(c==1):
-    print('prime')
-else:
+if(n==1):
     print('not a prime')
-    
+else:
+    for i in range(2,int(n**0.5)+1):
+        if(n%i==0):
+            print('not a prime')
+            break
+    else:
+        print('prime')
