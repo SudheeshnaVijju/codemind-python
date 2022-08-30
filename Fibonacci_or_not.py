2fa3893e7e03e2ef8993f959f03418eddf1a7f7c
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-08-30 19:03:26 +0530

    n=int(input()) x=0 y=1 while(1):     if(n==0 or n==1):         print('True')         break     else:         t=x         x=x+y         if(x==n):             print(True)             break         elif(x>n):             print(False)             break         y=t

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,17 +1,17 @@
 n=int(input())
 x=0
 y=1
-while(n):
-    if(n==x or n==y):
+while(1):
+    if(n==0 or n==1):
         print('True')
         break
     else:
         t=x
-        x=y+x
+        x=x+y
+        if(x==n):
+            print(True)
+            break
+        elif(x>n):
+            print(False)
+            break
         y=t
-        if(n==x):
-            print('True')
-            break
-        elif(n<x):
-            print('False')
-            break
