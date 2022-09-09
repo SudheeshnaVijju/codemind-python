20ba8f8ad2a0e13ca7c907b91866dd9ff6486cb1
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-09-09 20:46:13 +0530

    s=input() s=s.lower() s=list(s) c=0 for i in s:     if(s.count(i)==1 and i!=' '):         c+=1 print(c)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,12 +1,8 @@
 s=input()
-a=0
+s=s.lower()
+s=list(s)
+c=0
 for i in s:
-    c=0
-    if(ord(i)>=65 and ord(i)<=90):
-        i=chr(ord(i)+32)
-    for j in s:
-        if(i==j):
-            c+=1
-    if(c==1 and i!=' '):
-        a+=1
-print(a)
+    if(s.count(i)==1 and i!=' '):
+        c+=1
+print(c)
