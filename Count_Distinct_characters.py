95b69e76d2735c38b7713ad5436a1f0395bad36e
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-09-09 20:54:36 +0530

    s=input() s=set(list(s.lower())) c=0 for i in s:     if(i==' '):         c+=1 print(len(s)-c)

--- Count_Distinct_characters.py
+++ Count_Distinct_characters.py
@@ -1,10 +1,7 @@
 s=input()
-s=set(list(s))
-b=''
+s=set(list(s.lower()))
+c=0
 for i in s:
-    if(ord(i)>=65 and ord(i)<=90):
-        b=b+chr(ord(i)+32)
-    elif(i!=' '):
-        b+=i
-b=set(list(b))
-print(len(b))
+    if(i==' '):
+        c+=1
+print(len(s)-c)
