2a8d9cee0041216e1d39110c1a9cfe828661017f
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-10-06 20:18:25 +0530

    n=int(input()) a=list(map(int,input().split())) k=[] for i in a:     if i not in k:         k.append(i) for i in range(len(k)):     if(i!=len(k)-1):         print(k[i],end=' ')         print(a.count(k[i]),end=' ')     else:         print(k[i],end=' ')         print(a.count(k[i]))

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,9 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
 k=[]
-for i in range(len(a)):
-        if (a[i] not in k ):
-            k.append(a[i])
-for i in k:
-    print(i,end=' ')
-    print(a.count(i),end=' ')
+for i in a:
+    if i not in k:
+        k.append(i)
+for i in range(len(k)):
+    if(i!=len(k)-1):
+        print(k[i],end=' ')
+        print(a.count(k[i]),end=' ')
+    else:
+        print(k[i],end=' ')
+        print(a.count(k[i]))
