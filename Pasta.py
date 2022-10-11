08e9030c1bb9e47672cda77c980db805b30292e8
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-10-11 18:40:47 +0530

    a,b=map(int,input().split()) ls1=list(map(int,input().split())) ls2=list(map(int,input().split())) for i in ls2:     if i not in ls1:         print("No")         exit()     ls1.remove(i) print("Yes")

---
+++ Pasta.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+ls1=list(map(int,input().split()))
+ls2=list(map(int,input().split()))
+for i in ls2:
+    if i not in ls1:
+        print("No")
+        exit()
+    ls1.remove(i)
+print("Yes")
