e5344e8b5823ce3958ef918343832c574670408a
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-10-13 21:56:18 +0530

    s=input() s=s.split() for i in range(len(s)):     if(i%2==0):         p=s[i]         s[i]=p[::-1] print(*s)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,7 +1,7 @@
 s=input()
-k=s.split()
-for i in range(len(k)):
+s=s.split()
+for i in range(len(s)):
     if(i%2==0):
-        print(k[i][::-1],end=' ')
-    else:
-        print(k[i],end=' ')
+        p=s[i]
+        s[i]=p[::-1]
+print(*s)
