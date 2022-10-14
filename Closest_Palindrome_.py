6c87bd1432c9f689397ff077ed8d711996a144cd
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-10-14 21:53:18 +0530

    def ip(n):     if str(n)==str(n)[::-1]:         return 1     else:         return 0 n=int(input()) for i in range(1,n):     if(ip(n-i)==1 and ip(n+i)==1):         print(n-i,end=' ')         print(n+i,end=' ')         break     elif(ip(n-i)==1):         print(n-i)         break     elif(ip(n+i)==1):         print(n-i)         break

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,30 +1,17 @@
-def pan(n):
-    t=n
-    r=0
-    while(n):
-        r=r*10+(n%10)
-        n//=10
-    if(t==r):
+def ip(n):
+    if str(n)==str(n)[::-1]:
         return 1
     else:
         return 0
-
 n=int(input())
-t=n
-c,s,i=0,0,1
-while(1):
-    n=n+i
-    c+=1
-    if(pan(n)):
+for i in range(1,n):
+    if(ip(n-i)==1 and ip(n+i)==1):
+        print(n-i,end=' ')
+        print(n+i,end=' ')
         break
-while(t):
-    t=t-1
-    s+=1
-    if(pan(t)):
+    elif(ip(n-i)==1):
+        print(n-i)
         break
-if(s<c):
-    print(t)
-elif(c<s):
-    print(n)
-else:
-    print(t,n)
+    elif(ip(n+i)==1):
+        print(n-i)
+        break
