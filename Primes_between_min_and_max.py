fb626d6bd39d58f59471a5e0acb5544c858f5b7a
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-12-20 21:46:46 +0530

    def prime(n):     if(n==1):return 0     for i in range(2,int(n**0.5)+1):         if(n%i==0):return 0     return 1 n=int(input()) a=list(map(int,input().split())) m=min(a) ma=max(a) c=0 i=a.index(m) j=a.index(ma) if(i>j):i,j=j,i for o in range(i,j+1):     if(prime(a[o])):c+=1 print(c)

--- Primes_between_min_and_max.py
+++ Primes_between_min_and_max.py
@@ -1,24 +1,16 @@
-from math import sqrt
 def prime(n):
-    if(n==0):
-        return 0
-    if(n==1):
-        return 0
-    for i in range(2,int(sqrt(n))+1):
-        if(n%i==0):
-            return 0
+    if(n==1):return 0
+    for i in range(2,int(n**0.5)+1):
+        if(n%i==0):return 0
     return 1
 n=int(input())
 a=list(map(int,input().split()))
-m=a.index(min(a))
-ma=a.index(max(a))
+m=min(a)
+ma=max(a)
 c=0
-if(m>ma):
-    m,ma=ma,m
-for i in range(m,ma+1):
-    #if(i>=m and i<=ma and prime(i)):
-    if(prime(a[i]) and a[i]>0):
-        c+=1
+i=a.index(m)
+j=a.index(ma)
+if(i>j):i,j=j,i
+for o in range(i,j+1):
+    if(prime(a[o])):c+=1
 print(c)
-#print(m,ma)
-    
