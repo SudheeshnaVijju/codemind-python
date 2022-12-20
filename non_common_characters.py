800d68c172df84852bfcf903638db732003e763e
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-12-20 10:53:26 +0530

    s1=input().lower() s2=input().lower() k='' for i in s1:     if i not in s2 and i!=' ':         k+=i for i in s2:     if i not in s1 and i!=' ':         k+=i k=set(k) print(''.join(sorted(k)))

--- non_common_characters.py
+++ non_common_characters.py
@@ -1,8 +1,11 @@
-s1=list(input().lower())
-s2=list(input().lower())
-p='abcdefghijklmnopqrstuvwxyz'
-s=''
-for i in p:
-    if i not in s1 and i in s2 or i in s1 and i not in s2:
-        s+=i
-print(s)
+s1=input().lower()
+s2=input().lower()
+k=''
+for i in s1:
+    if i not in s2 and i!=' ':
+        k+=i
+for i in s2:
+    if i not in s1 and i!=' ':
+        k+=i
+k=set(k)
+print(''.join(sorted(k)))
