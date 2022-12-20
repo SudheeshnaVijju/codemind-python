bb044d363b9c8f7ea761a3529189db83b45fa207
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-12-20 22:18:04 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in range(1,n-1):     if(a[i-1]%2!=0 and a[i+1]%2==0):         c+=1         #print(a[i-1],a[i+1])     elif(a[i-1]%2==0 and a[i+1]%2!=0):         c+=1         #print(a[i-1],a[i+1]) if(a[0]%2!=0 and a[n-2]%2==0):     c+=1 elif(a[0]%2==0 and a[n-2]%2!=0):     c+=1 elif(a[n-1]%2==0 and a[1]%2!=0):c+=1 elif(a[n-1]%2!=0 and a[1]%2==0):c+=1 print(c)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -4,10 +4,14 @@ c=0
 for i in range(1,n-1):
     if(a[i-1]%2!=0 and a[i+1]%2==0):
         c+=1
-    if(a[i-1]%2==0 and a[i+1]%2!=0):
+        #print(a[i-1],a[i+1])
+    elif(a[i-1]%2==0 and a[i+1]%2!=0):
         c+=1
-if((a[n-2]%2==0 and a[0]%2!=0) or (a[n-2]%2!=0 and a[0]%2==0)):
+        #print(a[i-1],a[i+1])
+if(a[0]%2!=0 and a[n-2]%2==0):
     c+=1
-if((a[n-1]%2==0 and a[1]%2!=0) or (a[n-1]%2!=0 and a[1]%2==0)):
+elif(a[0]%2==0 and a[n-2]%2!=0):
     c+=1
+elif(a[n-1]%2==0 and a[1]%2!=0):c+=1
+elif(a[n-1]%2!=0 and a[1]%2==0):c+=1
 print(c)
