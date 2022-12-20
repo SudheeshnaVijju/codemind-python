52d9cfefaeb42a330e1368701ad5f4a1d15d2a07
Sudheeshna Vijju <97428825+SudheeshnaVijju@users.noreply.github.com>
2022-12-20 10:46:45 +0530

    s1=input().lower() s2,k=input().lower(),'' s,p='','' for i in s1:     s+=i for i in s2:     p+=i for i in s:     if i in p and i!=' ':         k+=i k=set(k) if(len(k)>0):     print(''.join(sorted(k))) else:     print("-1")

--- common_characters.py
+++ common_characters.py
@@ -1,11 +1,15 @@
-s1=list(input().lower())
-s2=list(input().lower())
-p='abcdefghijklmnopqrstuvwxyz'
-s=''
-for i in p:
-    if i in s1 and i in s2:
-        s+=i
-if s=='':
-    print('-1')
-    exit()
-print(s)
+s1=input().lower()
+s2,k=input().lower(),''
+s,p='',''
+for i in s1:
+    s+=i
+for i in s2:
+    p+=i
+for i in s:
+    if i in p and i!=' ':
+        k+=i
+k=set(k)
+if(len(k)>0):
+    print(''.join(sorted(k)))
+else:
+    print("-1")
